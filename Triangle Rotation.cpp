25e29199feadc8ac0c4a3df289cd6b194996461f
Imposter <ara404.github@gmail.com>
2024-08-15 11:30:04 +0530

    Update Triangle Rotation.cpp

--- Triangle Rotation.cpp
+++ Triangle Rotation.cpp
@@ -5,6 +5,19 @@
 
 using namespace std;
 
+void triangle::createMatrix()
+{
+    matrix[0][0] = x0;
+    matrix[1][0] = y0;
+    matrix[0][1] = x1;
+    matrix[1][1] = y1;
+    matrix[0][2] = x2;
+    matrix[1][2] = y2;
+    matrix[2][0] = 1;
+    matrix[2][1] = 1;
+    matrix[2][2] = 1;
+}
+
 void triangle::createRMatrix()
 {
     Rmatrix[0][0] = cos(theta);
